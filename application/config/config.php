135af655e7c21d86102535d82ed70fd47c3d7b65
j0rd3v <jomaroliverreyes@gmail.com>
2022-07-01 13:41:17 +0200

    Changed base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/project_folder/Assignments/Week006-Enrolment-Summary/';
+$config['base_url'] = 'http://localhost/projects/amauoed/Assignments/Week006-Enrolment-Summary';
 
 /*
 |--------------------------------------------------------------------------
